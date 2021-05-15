1f8328cd472bde6bb2b2eec18e6ba1e1f1bd2dd8
Rob Agar <robagar@fastmail.net>
2021-05-15 14:32:39 +0100

    object detection example

---
+++ examples/object_detection.py
@@ -0,0 +1,21 @@
+import jetson.inference
+from jetson_tello import h264_frame_to_cuda, FrameDecodeError
+
+from ast import literal_eval
+with open("frames.txt", "r") as f:
+    frames = [literal_eval(l) for l in f]
+
+net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
+
+i = 1
+for frame in frames:
+    try:
+        cuda, width, height = h264_frame_to_cuda(frame)
+
+        detections = net.Detect(cuda)
+
+        print(f'frame {i} detections:', detections)
+
+        i += 1
+    except FrameDecodeError:
+        pass    
