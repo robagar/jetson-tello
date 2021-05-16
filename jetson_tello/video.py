489df9d819f16881d0e6e773673a6b8423a17715
Rob Agar <robagar@fastmail.net>
2021-05-16 17:50:45 +0100

    video x,y to local azimuth & altitude

--- jetson_tello/video.py
+++ jetson_tello/video.py
@@ -1,6 +1,7 @@
 import jetson.utils
 import h264decoder # see https://github.com/DaWelter/h264decoder for installation instructions
 import numpy as np
+from tello_asyncio import VIDEO_WIDTH, VIDEO_HEIGHT
 
 class FrameDecodeError(Exception):
     pass
@@ -21,3 +22,16 @@ def h264_frame_to_cuda(frame):
     array, width, height = h264_frame_to_numpy_array(frame)
     cuda = jetson.utils.cudaFromNumpy(array)
     return cuda, width, height
+
+# Tello camera field of view measured (roughly) in degrees
+CAMERA_HORIZONTAL_HALF_FOV = 26.9
+CAMERA_VERTICAL_HALF_FOV = 20.2
+
+HALF_VIDEO_WIDTH = VIDEO_WIDTH / 2
+HALF_VIDEO_HEIGHT = VIDEO_HEIGHT / 2
+
+def video_x_to_local_azimuth(x, y):
+    return CAMERA_HORIZONTAL_HALF_FOV * (y - HALF_VIDEO_WIDTH) / HALF_VIDEO_WIDTH
+
+def video_y_to_local_altitude(x, y):
+    return -CAMERA_VERTICAL_HALF_FOV * (y - HALF_VIDEO_HEIGHT) / HALF_VIDEO_HEIGHT
