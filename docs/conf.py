6e42e4411fb3807792b24915a5c399c784b5cd78
Rob Agar <robagar@fastmail.net>
2021-05-31 14:42:16 +0100

    1.1.8 more RTD trial + error

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Rob Agar'
 author = 'Rob Agar'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.7'
+release = '1.1.8'
 
 
 # -- General configuration ---------------------------------------------------
