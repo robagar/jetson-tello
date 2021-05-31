c22fdf935ec5628419e12cd5e055089e1a7a0c07
Rob Agar <robagar@fastmail.net>
2021-05-31 06:47:22 +0100

    DecodedFrame docs

--- jetson_tello/video/types.py
+++ jetson_tello/video/types.py
@@ -1,3 +1,8 @@
 from collections import namedtuple
 
 DecodedFrame = namedtuple('DecodedFrame', 'number width height data')
+DecodedFrame.__doc__ = 'Video frame data and information'
+DecodedFrame.number.__doc__ = 'Position in the sequence of successfully decoded frames'
+DecodedFrame.width.__doc__ = 'Frame image width in pixels'
+DecodedFrame.height.__doc__ = 'Frame image height in pixels'
+DecodedFrame.data.__doc__ = 'Decoded frame data as `bytes`'
