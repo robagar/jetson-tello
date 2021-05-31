200d153779182ab90fcacfc673db307befe0857d
Rob Agar <robagar@fastmail.net>
2021-05-31 13:47:15 +0100

    1.1.7

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Rob Agar'
 author = 'Rob Agar'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.6'
+release = '1.1.7'
 
 
 # -- General configuration ---------------------------------------------------
